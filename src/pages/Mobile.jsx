eba13c80648b1f4d05c6910fed2c7976f981141e
0dot77 <yty0706@gmail.com>
2022-07-14 22:17:15 +0900

    모바일 test 페이지 제작

--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components';
+const MainContianer = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Notice = styled.p`
+  font-size: 2rem;
+  font-style: italic;
+  text-align: center;
+`;
+
 export default function Mobile() {
   return (
-    <>
-      <h1>MOBILE</h1>
-    </>
+    <MainContianer>
+      <Notice>MOBILE IS NOT SUPPORTED, PLEASE ACCESS THE DESKTOP!</Notice>
+    </MainContianer>
   );
 }
